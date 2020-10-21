75eda4552b5f0ab9d5cfad96835be7039edc8c4d
fbzr <fabricio.spb@gmail.com>
2020-10-20 17:27:07 -0700

    add passport.serializeUser and passport.deserializeUser - login with facebook working!

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ const app = express();
 
 app.use(passport.initialize());
 
+/* The user id (you provide as the second argument of the done function) is saved in the session and is later used to retrieve the whole object via the deserializeUser function. */
+passport.serializeUser(function (user, done) {
+  // done(null, user.id);
+
+  // saving the whole user object just for test
+  done(null, user);
+});
+
+// passport.deserializeUser(function (id, done) {
+// receiving the whole user just for test
+passport.deserializeUser(function (user, done) {
+  // User.findById(id, function (err, user) {
+  //   done(err, user);
+  // });
+  console.log(`Deserialize user.\nUser: ${user}`);
+  done(err, user);
+});
+
 passport.use(
   new FacebookStrategy(
     {
@@ -28,7 +46,7 @@ passport.use(
       // });
 
       console.log(profile);
-      cb(() => console.log("error"), profile);
+      cb(null, profile);
     },
   ),
 );
