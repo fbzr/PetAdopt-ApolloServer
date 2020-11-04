ace20f93a4ba3159753d71a96548d9a5d8413a91
fbzr <fabricio.spb@gmail.com>
2020-11-04 15:19:58 -0800

    add res json obj

--- server/express.js
+++ server/express.js
@@ -30,10 +30,10 @@ app.get("/", (req, res) => {
   console.log("***REQ SESSION***\n", req.session);
   if (req.isAuthenticated()) {
     console.log("***REQ USER***\n", req.user);
-    return res.send("AUTHENTICATED");
+    return res.json({message: "AUTHENTICATED", success: true, user: req.user});
   }
-
-  res.send("API WORKING! NOT AUTHENTICATED");
+  
+  res.status(401).json({message: "API WORKING! NOT AUTHENTICATED", success: false});
 });
 
 module.exports = app;
