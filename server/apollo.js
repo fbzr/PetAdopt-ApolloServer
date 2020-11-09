89dccf9f6b1e1cbaa0b7141b757e554bf4348e95
fbzr <fabricio.spb@gmail.com>
2020-11-09 06:37:37 -0800

    add playground config to apollo server

--- server/apollo.js
+++ server/apollo.js
@@ -13,12 +13,15 @@ const apolloServer = new ApolloServer({
       throw new AuthenticationError("Authentication required");
     }
   },
-  playground: {
-    // to be able to access req properties in context function
-    settings: {
-      "request.credentials": "include",
-    },
-  },
+  playground:
+    process.env.NODE_ENV === "production"
+      ? false
+      : {
+          // to be able to access req properties in context function
+          settings: {
+            "request.credentials": "include",
+          },
+        },
 });
 
 module.exports = apolloServer;
