69fda4d8ed265b00c03b2cb6ecf33cac1131b3d9
fbzr <fabricio.spb@gmail.com>
2020-11-09 15:57:08 -0800

    add cors config to apolloserver

--- server/express.js
+++ server/express.js
@@ -32,23 +32,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 passport(app);
 
-app.use("/graphql", (req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "content-type, authorization, content-length, x-requested-with, accept, origin"
-  );
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.header("Allow", "POST, GET, OPTIONS");
-  res.header("Access-Control-Allow-Origin", "*");
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
+apolloServer.applyMiddleware({
+  app,
+  cors: {
+    origin: process.env.CLIENT_URL || "http://localhost:4200",
+    credentials: true,
+  },
 });
-
-apolloServer.applyMiddleware({ app });
 
 app.get("/", (req, res) => {
   console.log("***REQ SESSION***\n", req.session);
